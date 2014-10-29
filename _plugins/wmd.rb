9a457abe24df890ed17cd822a812ab2e1e412644
Lee Worden <worden.lee@gmail.com>
2014-10-28 21:59:23 -0700

    wmd pre and post processing built into jekyll now!

--- _plugins/wmd.rb
+++ _plugins/wmd.rb
@@ -1,8 +1,25 @@
+def pipe_text_through( text, command )
+	open('|'+command, 'w+') do |subprocess|
+		subprocess.write( text )
+		subprocess.close_write
+		return subprocess.read
+	end
+end
+
 module Jekyll
 	class WMDGenerator < Jekyll::Generator
 		safe true
 		priority :low
+		@project_lookup = Hash.new
 		
+		def self.record_project( title, value )
+			@project_lookup[title] = value
+		end
+
+		def self.lookup_project( title )
+			@project_lookup[title]
+		end
+
 		def initialize(config)
 			config['convert_wmd'] ||= true
 		end
@@ -17,17 +34,30 @@ module Jekyll
 		end
 
 		def convertWmd(post)
-			post.content = "WMD Converted Content [link here](http://google.com)\n\n" + post.content
-			puts "name is #{post.name}"
 			post.name.gsub!(/\.wmd$/, '')
-			post.name.gsub!(/\.markdown/, '.foo.markdown')
-			puts "name becomes #{post.name}"
-			puts "slug is #{post.slug}"
+			WMDGenerator.record_project(post.title, post.name)
 			post.slug.gsub!(/\.wmd$/, '')
-			post.slug.gsub!(/\.markdown/, '.foo')
-			puts "slug becomes #{post.slug}"
-			puts "ext is #{post.ext}"
-			post.ext = '.markdown'
+			post.ext = post.slug.match(/\..*?$/)[0]
+			post.slug.gsub!(/\..*?$/, '')
+			puts "Calling wmd.php --pre on #{post.slug}"
+			post.content = pipe_text_through( post.content, "php /usr/local/src/workingwiki/wmd/wmd.php --pre --default-project-name=#{post.name} --cache-dir=/usr/local/src/jekyll-test/github-pages-sandbox/wmd_files --process-inline-math" )
+		end
+	end
+
+	module WMDFilter
+		def wmd_postprocess(text)
+			if ! text.match(/UNIQ/)
+				return text
+			end
+			title = @context.registers[:page]['title']
+			name = WMDGenerator.lookup_project(title)
+			puts "Calling wmd.php --post on #{name}"
+			baseurl = @context.registers[:site].baseurl
+			text = pipe_text_through( text, "php /usr/local/src/workingwiki/wmd/wmd.php --post --default-project-name=#{name} --cache-dir=/usr/local/src/jekyll-test/github-pages-sandbox/wmd_files --project-file-base-url=#{baseurl}/wmd_files" )
+			@context.registers[:site].read_directories( "wmd_files/#{name}" )
+			return text
 		end
 	end
 end
+
+Liquid::Template.register_filter(Jekyll::WMDFilter)
